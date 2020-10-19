0f10fae7a5bc105c4620d316fbc7a97db34533a7
rokpopov <rokpopov90@gmail.com>
2020-10-19 10:43:06 +0200

    Create a small quiz

--- app.py
+++ app.py
@@ -24,4 +24,24 @@ while i <= number:
     print(str(i) + "!")
     i += 1
 
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times.")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+
+answer = int(input())
+    if answer == 1:
+        print("Completed, have a nice day!")
+    elif answer == 2:
+        print("Please, try again!")
+    elif answer == 3:
+        print("Please, try again!")
+    elif answer == 4:
+        print("Please, try again!")
+    else:
+        print("Please, answer with a number from 1 to 4!")
+
+
 print("Completed, have a nice day!")
